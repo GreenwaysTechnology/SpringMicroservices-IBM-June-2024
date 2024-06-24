890dd7c98e743ddb19568a440ee65918f6cf4d99
subugee <sasubramanian_md@hotmail.com>
2024-06-24 13:24:41 +0530

    non blocking web app

---
+++ nonblockingwebapp/src/main/java/com/ibm/nonblocking/NonblockingwebappApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.nonblocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonblockingwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonblockingwebappApplication.class, args);
+	}
+
+}
---
+++ nonblockingwebapp/src/test/java/com/ibm/nonblocking/NonblockingwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.nonblocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonblockingwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
